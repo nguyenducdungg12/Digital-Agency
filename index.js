ae51b89e16fefe94deb06087ed5e367a2b9f5b22
Nguyen duc dung <nguyenducdungg12@gmail.com>
2020-08-21 21:39:00 +0700

    finsh html & css

---
+++ index.js
@@ -0,0 +1,48 @@
+$('.owl-carousel0').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:4
+        }
+    }
+});
+$('.owl-carousel1').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+});
+$('.owl-carousel2').owlCarousel({
+    loop:true,
+    margin:0,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:3
+        }
+    }
+})
